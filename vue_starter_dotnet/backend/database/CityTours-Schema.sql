29887ab3927e4e8483f28e849360f51202ce77d2
Alex Pitcairn <apitcairn3@gmail.com>
2020-04-08 16:57:29 -0400

    adding addresses

--- vue_starter_dotnet/backend/database/CityTours-Schema.sql
+++ vue_starter_dotnet/backend/database/CityTours-Schema.sql
@@ -37,8 +37,10 @@ CREATE TABLE Users(
 CREATE TABLE Landmarks(
 	landmark_id				INT IDENTITY(1,1),
 	landmark_name			VARCHAR(100)	NOT NULL,
+	street_address			VARCHAR(100)	NOT NULL,
 	city					VARCHAR(100)	NOT NULL,
 	state					VARCHAR(2)		NOT NULL,
+	zip_code				INTEGER			NOT NULL,
 	days_open				VARCHAR(100)	NOT NULL, -- eg Mon-Fri, Mon Wed Fri, Mon - Sun
     hours_of_operation		VARCHAR(100)	NOT NULL, -- eg 8AM - 8PM, 8AM - 12PM : 1PM - 5PM
 	category_id				INTEGER			NOT NULL,
@@ -77,22 +79,22 @@ SET IDENTITY_INSERT LandmarkCategories ON;
 SET IDENTITY_INSERT LandmarkCategories OFF;
 
 SET IDENTITY_INSERT Landmarks ON;
-	INSERT INTO Landmarks (landmark_id, landmark_name, city, state, days_open, hours_of_operation, category_id, description ) 
-	VALUES	(1, 'Skyline Chili', 'Cincinnati', 'OH',  'MON-FRI', '8AM - 10PM', 1, 'Cincinnati style chili'),
-			(2, 'Fountain Square', 'Cincinnati', 'OH', 'SAT SUN', '8AM - 6PM', 2, 'Center of the city'),
-			(3, 'Musem Center at Union Terminal', 'Cincinnati', 'OH', 'SUN-SAT', '10AM -6PM', 3, 'Cincinnati Museum Center at Union Terminal is home to the Cincinnati History Museum, Duke Energy Children''s Museum, Museum of Natural History & Science, and Robert D. Lindner Family OMNIMAX Theater.'),
-			(4, 'Sawyer Point Park & Yeatman''s Cove', 'Cincinnati', 'OH', 'SUN-SAT', '9AM-7PM', 2, 'Sawyer Point Park & Yeatman''s Cove are a pair of side-by-side parks on the riverfront of downtown Cincinnati, Ohio, United States. The two linear parks stretch one mile along the north shore of the Ohio River. Since 2012, the parks have been the location for the annual Bunbury Music Festival.'),
-			(5, 'Ault Park', 'Cincinnati', 'OH', 'SUN-SAT', '9AM-7PM', 2, 'Ault Park is the fourth-largest park in Cincinnati at 223.949 acres, owned and operated by the Cincinnati Park Board. It lies in the Mount Lookout neighborhood on the city''s east side.'),
-			(6, 'Findlay Market', 'Cincinnati', 'OH', 'TUES-SUN', '7AM-6PM', 1, 'Longtime hub of vendors selling meat, produce & other local foods, plus a seasonal farmers'' market.'),
-			(7, 'Rhinegeist Brewery', 'Cincinnati', 'OH', 'SUN-SAT', '11AM-9PM', 1, 'Taproom located in a historic bottling plant offering house-brewed beers, Ping-Pong, cornhole & TVs.'),
-			(8, 'Contemporary Arts Center', 'Cincinnati', 'OH', 'TUES-SUN', '10AM-6PM', 3, 'The Contemporary Arts Center is a contemporary art museum in Cincinnati, Ohio and one of the first contemporary art institutions in the United States. The CAC is a non-collecting museum that focuses on new developments in painting, sculpture, photography, architecture, performance art and new media.'),
-			(9, 'Cincinnati Art Museum', 'Cincinnati', 'OH', 'TUES-SUN', '10AM-6PM', 3, 'The Cincinnati Art Museum is an art museum in the Eden Park neighborhood of Cincinnati, Ohio. Founded in 1881, it was the first purpose-built art museum west of the Alleghenies, and is one of the oldest in the United States.'),
-			(10, 'Cincinnati Zoo & Botanical Garden', 'Cincinnati', 'OH', 'SUN-SAT', '10AM-7PM', 2, 'Cincinnati Zoo & Botanical Garden is the fifth-oldest zoo in the United States, opening in 1875, after the Philadelphia Zoo, the Roger Williams Park Zoo, the Lincoln Park Zoo, and the Central Park Zoo. It is located in the Avondale neighborhood of Cincinnati, Ohio.'),
-			(11, 'Washington Park', 'Cincinnati', 'OH', 'SUN-SAT', '10AM-7PM', 2, 'Washington Park is bounded by West 12th, Race and Elm Streets in the Over-the-Rhine neighborhood of Cincinnati, Ohio, United States. The park is owned and operated by the Cincinnati Park Board. The 6-acre park served as Presbyterian and Episcopal cemeteries before it was acquired by the city from 1858 to 1863.'),
-			(12, 'Taft Museum of Art', 'Cincinnati', 'OH', 'TUES-SUN', '10AM-6PM', 3, 'The Taft Museum of Art is housed in the 200-year-old historic house at 316 Pike Street. The house – the oldest domestic wooden structure in downtown Cincinnati in situ – was built about 1820 and housed several prominent Cincinnatians, including Martin Baum, Nicholas Longworth, David Sinton, Anna Sinton Taft and Charles Phelps Taft. It now holds a fine art collection, is on the National Register of Historic Places listings, and is a contributing property to the Lytle Park Historic District.'),
-			(13, 'MadTree Brewing', 'Cincinnati', 'OH', 'SUN-SAT', '11AM-8PM', 1, 'Bustling taproom offering craft beers, wood-fired pizzas, growler fills & keg sales, plus a patio.'),
-			(14, 'Great American Ball Park', 'Cincinnati', 'OH', 'OPEN FOR EVENTS', 'TIMES VARY', 4, 'Great American Ball Park is a baseball stadium in Cincinnati, Ohio, which is the home field of Major League Baseball''s Cincinnati Reds. It opened in 2003, replacing Cinergy Field, their home field from 1970 to 2002. The park''s name comes from Great American Insurance Group.'),
-			(15, 'Paul Brown Stadium', 'Cincinnati', 'OH', 'OPEN FOR EVENTS', 'TIMES VARY', 4, 'Paul Brown Stadium is an outdoor football stadium in Cincinnati, Ohio. It is the home venue of the Cincinnati Bengals of the National Football League and opened on August 19, 2000.');
+	INSERT INTO Landmarks (landmark_id, landmark_name, street_address, city, state, zip_code, days_open, hours_of_operation, category_id, description ) 
+	VALUES	(1, 'Skyline Chili', '290 Ludlow Ave', 'Cincinnati', 'OH', 45220,  'MON-FRI', '8AM - 10PM', 1, 'Cincinnati style chili'),
+			(2, 'Fountain Square','520 Vine St', 'Cincinnati', 'OH', 45202, 'SAT SUN', '8AM - 6PM', 2, 'Center of the city'),
+			(3, 'Musem Center at Union Terminal','1301 Western Ave', 'Cincinnati', 'OH', 45203, 'SUN-SAT', '10AM -6PM', 3, 'Cincinnati Museum Center at Union Terminal is home to the Cincinnati History Museum, Duke Energy Children''s Museum, Museum of Natural History & Science, and Robert D. Lindner Family OMNIMAX Theater.'),
+			(4, 'Sawyer Point Park & Yeatman''s Cove','705 E Pete Rose Way', 'Cincinnati', 45202, 'OH', 'SUN-SAT', '9AM-7PM', 2, 'Sawyer Point Park & Yeatman''s Cove are a pair of side-by-side parks on the riverfront of downtown Cincinnati, Ohio, United States. The two linear parks stretch one mile along the north shore of the Ohio River. Since 2012, the parks have been the location for the annual Bunbury Music Festival.'),
+			(5, 'Ault Park', '5090 Observatory Ave', 'Cincinnati', 'OH', 45208, 'SUN-SAT', '9AM-7PM', 2, 'Ault Park is the fourth-largest park in Cincinnati at 223.949 acres, owned and operated by the Cincinnati Park Board. It lies in the Mount Lookout neighborhood on the city''s east side.'),
+			(6, 'Findlay Market', '1801 Race St', 'Cincinnati', 'OH', 45202, 'TUES-SUN', '7AM-6PM', 1, 'Longtime hub of vendors selling meat, produce & other local foods, plus a seasonal farmers'' market.'),
+			(7, 'Rhinegeist Brewery', '1910 Elm St', 'Cincinnati', 'OH', 45202, 'SUN-SAT', '11AM-9PM', 1, 'Taproom located in a historic bottling plant offering house-brewed beers, Ping-Pong, cornhole & TVs.'),
+			(8, 'Contemporary Arts Center','44 E 6th St', 'Cincinnati', 'OH', 45202, 'TUES-SUN', '10AM-6PM', 3, 'The Contemporary Arts Center is a contemporary art museum in Cincinnati, Ohio and one of the first contemporary art institutions in the United States. The CAC is a non-collecting museum that focuses on new developments in painting, sculpture, photography, architecture, performance art and new media.'),
+			(9, 'Cincinnati Art Museum', '953 Eden Park Dr', 'Cincinnati', 'OH', 45202, 'TUES-SUN', '10AM-6PM', 3, 'The Cincinnati Art Museum is an art museum in the Eden Park neighborhood of Cincinnati, Ohio. Founded in 1881, it was the first purpose-built art museum west of the Alleghenies, and is one of the oldest in the United States.'),
+			(10, 'Cincinnati Zoo & Botanical Garden', '3400 Vine St', 'Cincinnati', 'OH', 45220, 'SUN-SAT', '10AM-7PM', 2, 'Cincinnati Zoo & Botanical Garden is the fifth-oldest zoo in the United States, opening in 1875, after the Philadelphia Zoo, the Roger Williams Park Zoo, the Lincoln Park Zoo, and the Central Park Zoo. It is located in the Avondale neighborhood of Cincinnati, Ohio.'),
+			(11, 'Washington Park', '1230 Elm St', 'Cincinnati', 'OH', 45202, 'SUN-SAT', '10AM-11PM', 2, 'Washington Park is bounded by West 12th, Race and Elm Streets in the Over-the-Rhine neighborhood of Cincinnati, Ohio, United States. The park is owned and operated by the Cincinnati Park Board. The 6-acre park served as Presbyterian and Episcopal cemeteries before it was acquired by the city from 1858 to 1863.'),
+			(12, 'Taft Museum of Art', '316 Pike St', 'Cincinnati', 'OH', 45202, 'TUES-SUN', '10AM-6PM', 3, 'The Taft Museum of Art is housed in the 200-year-old historic house at 316 Pike Street. The house – the oldest domestic wooden structure in downtown Cincinnati in situ – was built about 1820 and housed several prominent Cincinnatians, including Martin Baum, Nicholas Longworth, David Sinton, Anna Sinton Taft and Charles Phelps Taft. It now holds a fine art collection, is on the National Register of Historic Places listings, and is a contributing property to the Lytle Park Historic District.'),
+			(13, 'MadTree Brewing','3301 Madison Rd', 'Cincinnati', 'OH', 45209, 'SUN-SAT', '11AM-8PM', 1, 'Bustling taproom offering craft beers, wood-fired pizzas, growler fills & keg sales, plus a patio.'),
+			(14, 'Great American Ball Park','100 Joe Nuxhall Way', 'Cincinnati', 'OH', 45202, 'OPEN FOR EVENTS', 'TIMES VARY', 4, 'Great American Ball Park is a baseball stadium in Cincinnati, Ohio, which is the home field of Major League Baseball''s Cincinnati Reds. It opened in 2003, replacing Cinergy Field, their home field from 1970 to 2002. The park''s name comes from Great American Insurance Group.'),
+			(15, 'Paul Brown Stadium', '1 Paul Brown Stadium', 'Cincinnati', 'OH', 45202, 'OPEN FOR EVENTS', 'TIMES VARY', 4, 'Paul Brown Stadium is an outdoor football stadium in Cincinnati, Ohio. It is the home venue of the Cincinnati Bengals of the National Football League and opened on August 19, 2000.');
 			
 SET IDENTITY_INSERT Landmarks OFF;
 
@@ -230,8 +232,10 @@ SELECT
 	A.days_open,
 	A.hours_of_operation,
 	A.description AS landmark_description,
+	A.street_address,
 	A.city,
 	A.state,
+	A.zip_code,
 	B.category_id,
 	B.category_name,
 	C.image_id,
