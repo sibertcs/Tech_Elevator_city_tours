cc66ecc2dbd04125dcbcb6b322741bb793a999bc
Jack Egan <jackegan2424@gmail.com>
2020-04-12 16:50:20 -0400

    manage page can now save, has dropdown for itineraries and loads the landmarks including one if added client side

--- vue_starter_dotnet/frontend/src/views/ManageItinerary.vue
+++ vue_starter_dotnet/frontend/src/views/ManageItinerary.vue
@@ -1,54 +1,83 @@
 <template>
-  <form>
-    <div class="manage-itinerary card">
-      <div class="form-group">
-        <label for="itineraryName">Itinerary Name</label>
-        <input
-          type="text"
-          class="form-control"
-          id="itineraryName"
-          placeholder="Name"
-          v-model.trim="itinerary.name"
-        />
-        <div>Please provide a valid itinerary name.</div>
+  <div id="manageItineraryWrapper">
+    <div id="serverResponse" v-if="serverMessage.length > 0">
+      <span>{{serverMessage}}</span>
+    </div>    
+    <form @submit.prevent="saveChanges">
+      <div class="manage-itinerary card">
+        <div class="form-group">
+          <label for="itineraryName">Itinerary Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="itineraryName"
+            placeholder="Name"
+            v-model.trim="itinerary.name"
+          />
+          <div>Please provide a valid itinerary name.</div>
+        </div>
+        <div class="form-group">
+          <label for="startDate">Date</label>
+          <input
+            v-if="itinerary.itineraryDate != null"
+            type="date"
+            class="form-control"
+            id="startDate"
+            placeholder="YYYY/MM/DD"
+            v-model.trim="itinerary.itineraryDate.split('T')[0]"
+          />
+          <div>Please provide a valid date.</div>
+        </div>
+        <div class="form-group">
+          <label for="startingAddress">Starting Address</label>
+          <input
+            type="text"
+            class="form-control"
+            id="startingAddress"
+            placeholder="1234 Main St"
+            v-model.trim="itinerary.startingLocation"
+          />
+          <div>Please provide a valid starting address.</div>
+        </div>
+        <div id="landmarksWrapper">
+          <div class="landmark-itinerary-object" v-for="landmark in itinerary.landmarks" v-bind:key="landmark.landmarkID">
+              <div class="landmark-sort-order-wrapper">
+                <span>{{landmark.sortOrder}}</span>
+              </div>
+              <div class="landmark-name-wrapper">
+                <span>{{landmark.name}}</span>
+              </div>
+              <div class="landmark-move-up-wrapper">
+                <b-button >UP</b-button>
+              </div>
+              <div class="landmark-move-down-wrapper">
+                <b-button >DOWN</b-button>
+              </div>
+              <div class="landmark-remove-wrapper">
+                <b-button >REMOVE</b-button>
+              </div>
+          </div>
+        </div>
+        <div>
+          <select id="itineraryDropDown" v-on:change="onSelectChange">
+            <option
+              v-for="itineraryOption in userItineraries"
+              v-bind:key="itineraryOption.itineraryID"
+              v-bind:value="itineraryOption.itineraryID"
+              v-text="itineraryOption.name"
+            ></option>
+          </select>
+        </div>
+
+        <div class="float-right">
+          <button
+            type="submit"
+            class="btn btn-lg btn-primary btn-block btn-secondary"
+          >Save Itinerary</button>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="startDate">Date</label>
-        <input
-          type="date"
-          class="form-control"
-          id="startDate"
-          placeholder="YYYY/MM/DD"
-          v-model.trim="itinerary.itineraryDate.split('T')[0]"
-        />
-        <div>Please provide a valid date.</div>
-      </div>
-      <div class="form-group">
-        <label for="startingAddress">Starting Address</label>
-        <input
-          type="text"
-          class="form-control"
-          id="startingAddress"
-          placeholder="1234 Main St"
-          v-model.trim="itinerary.startingLocation"
-        />
-        <div>Please provide a valid starting address.</div>
-      </div>
-      <div>
-        <select id="itineraryDropDown" v-model="getUserItinerary" v-on:change="onSelectChange">
-            
-          <option
-            v-for="itineraryOption in userItineraries"
-            v-bind:key="itineraryOption.itineraryID"
-          >{{itineraryOption.itineraryName}}</option>
-        </select>
-      </div>
-      
-      <div class="float-right">
-        <button type="submit" class="btn btn-lg btn-primary btn-block btn-secondary">Save Itinerary</button>
-      </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -59,13 +88,18 @@ export default {
   data() {
     return {
       itinerary: Object,
-      userItineraries: []
+      userItineraries: [Object],
+      sortOrderChanged: false,
+      itineraryChanged: false,
+      serverMessage: "",
+      incomingLandmarkID: Number
     };
   },
   methods: {
     getItinerary() {
       const userID = auth.getUser().id;
       const apiEndpoint = `getuseritinerary/${userID}`;
+      console.log("fetching: getuseritinerary");
       fetch(`${process.env.VUE_APP_REMOTE_API_LANDMARKS}/${apiEndpoint}`, {
         method: "GET",
         headers: {
@@ -80,13 +114,37 @@ export default {
         })
         .then(data => {
           this.itinerary = data;
+          this.getIncomingLandmark(); //oversight: this needs to add
         });
     },
-    onSelectChange(){
-        // get the new selected itineraryID from drop down 
-        // set this.itinerary = userItineraries.find(itinerary => itinerary.id == new selected id)
-        // call setSelectedItinerary()
-        // call getItinerary()
+    saveChanges() {
+      this.EditItinerary();
+    },
+    EditItinerary() {
+      //const userID = auth.getUser().id;
+      const apiEndpoint = `edititinerary`;
+      fetch(`${process.env.VUE_APP_REMOTE_API_LANDMARKS}/${apiEndpoint}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.getUser()
+        },
+        body: JSON.stringify(this.itinerary)
+      })
+        .then(response => {
+          if (response.ok) {
+            return response;
+          }
+        })
+        .then(message => {
+          this.serverMessage = message;
+        });
+    },
+    onSelectChange() {
+      // get the new selected itineraryID from drop down
+      // set this.itinerary = userItineraries.find(itinerary => itinerary.id == new selected id)
+      // call setSelectedItinerary()
+      // call getItinerary()
     },
     getUserItinerary() {
       const userID = auth.getUser().id;
@@ -104,12 +162,13 @@ export default {
           }
         })
         .then(data => {
-          this.userItineraries = data;
+          this.itinerary = data;
         });
     },
     getUserItineraries() {
       const userID = auth.getUser().id;
       const apiEndpoint = `getusersitineraries/${userID}`;
+      console.log("fetching: getusersitineraries" );
       fetch(`${process.env.VUE_APP_REMOTE_API_LANDMARKS}/${apiEndpoint}`, {
         method: "GET",
         headers: {
@@ -123,7 +182,9 @@ export default {
           }
         })
         .then(data => {
-          this.itinerary = data;
+          //alert(data);
+          this.userItineraries = data;
+          //alert(this.userItineraries);
         });
     },
     getItineraryById(itineraryID) {
@@ -161,13 +222,55 @@ export default {
           }
         })
         .catch(err => console.error(err));
+    },
+    loadData() {
+      console.log("getItinerary");
+      this.getItinerary();
+      console.log("getUserItineraries");
+      this.getUserItineraries();
+      // console.log("getIncomingLandmark");
+      // this.getIncomingLandmark();
+    },
+    getIncomingLandmark() {
+      console.log(JSON.stringify(this.itinerary));
+      if (this.$route.params.landmark_id != null) {
+        this.incomingLandmarkID = this.$route.params.landmark_id;
+
+        //if (this.itinerary.landmarks != [] && this.itinerary.landmarks != null) {
+          if (
+            this.itinerary.landmarks.find(landmark => {
+              return landmark.landmarkID == this.incomingLandmarkID;
+            }) == null
+          ) {
+            console.log("fetching: getlandmark");
+            fetch(
+              `${process.env.VUE_APP_REMOTE_API_LANDMARKS}/getlandmark/${this.incomingLandmarkID}`
+            )
+              .then(response => {
+                if (response.ok) {
+                  return response.json();
+                }
+              })
+              .then(data => {
+                let newLandmark = {
+                  "landmarkID": data.landmarkID,
+                  "name": data.name,
+                  "sortOrder": (this.landmarks == null ? 1 : this.landmarks.length + 1),
+                  "landmark": data,
+                  "addedByClient": true,
+                  "hasLandmarkObject": true
+                }
+                this.itinerary.landmarks.push(newLandmark);
+              })
+              .catch(err => console.error(err));
+          }
+        }
+      //}
     }
   },
-
   created() {
-    this.getItinerary();
-  },
-
+    this.loadData();
+  }
 };
 </script>
 
